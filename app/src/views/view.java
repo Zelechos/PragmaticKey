88c6b93e35fb6db4ee3c0b5344188a250ca04cd4
Pragmatic Coder <zelechos@hotmail.com>
2022-05-16 22:41:13 -0400

    v0.0.0

--- app/src/views/view.java
+++ app/src/views/view.java
@@ -1,37 +1,56 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * An <Alex Tumiri> production
  */
 package views;
 
 import com.sun.awt.AWTUtilities;
 import java.awt.Shape;
+import java.awt.Toolkit;
 import java.awt.geom.RoundRectangle2D;
 
 /**
  *
- * @author Zelec
+ * @author Alex Tumiri
  */
 public class view extends javax.swing.JFrame {
 
-    /**
-     * Creates new form view
-     */
     public view() {
         beauty(this);
         initComponents();
         borderRadiusWindow(this);
+        positionWindow(this);
     }
     
+    /**
+     * subrutina para quitarle el marco por defecto a la ventana
+     * @param instance 
+     */
     public static void beauty(view instance){
         instance.setUndecorated(true);
-        instance.setLocation(100,100);
     }
     
+    /**
+     * subrutina para renderizar los bordes de la ventana
+     * @param instance 
+     */
     public static void borderRadiusWindow(view instance){
-        Shape borderRadius = new RoundRectangle2D.Double(0,0,instance.getBounds().width, instance.getBounds().height,50,50); 
+        Shape borderRadius = new RoundRectangle2D.Double(0,0,instance.getBounds().width, instance.getBounds().height,25,25); 
         AWTUtilities.setWindowShape(instance, borderRadius);
+    }
+    
+    /**
+     * subrutina para posicionar la ventana dinamicamente segun la pantalla
+     * @param instance 
+     */
+    public static void positionWindow(view instance){
+        int heightScreen = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        int widthScreen = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        
+        // [width => 210,  height => 111] size of JFrame
+        int positionHeight = heightScreen - 141;
+        int positionWidth = widthScreen - 230;
+        
+        instance.setLocation(positionWidth, positionHeight);
     }
 
     /**
@@ -48,12 +67,13 @@ public class view extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        panel1.setBackground(new java.awt.Color(255, 48, 98));
+        panel1.setBackground(new java.awt.Color(0, 5, 28));
         panel1.setForeground(new java.awt.Color(255, 255, 255));
 
-        app.setFont(new java.awt.Font("Candara Light", 0, 24)); // NOI18N
+        app.setFont(new java.awt.Font("Candara Light", 0, 60)); // NOI18N
+        app.setForeground(new java.awt.Color(255, 255, 255));
         app.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        app.setText("PragmaticKey");
+        app.setText("<caster/>");
         app.setToolTipText("");
 
         javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
@@ -61,25 +81,23 @@ public class view extends javax.swing.JFrame {
         panel1Layout.setHorizontalGroup(
             panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panel1Layout.createSequentialGroup()
-                .addGap(121, 121, 121)
-                .addComponent(app, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(126, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(app, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(11, Short.MAX_VALUE))
         );
         panel1Layout.setVerticalGroup(
             panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
-                .addContainerGap(66, Short.MAX_VALUE)
-                .addComponent(app, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(58, 58, 58))
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(app, javax.swing.GroupLayout.PREFERRED_SIZE, 67, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(33, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
